6819a98e07cb283514be7a304e3abdef463c8187
mark a. foltz <spuddybuddy@github.com>
2025-05-20 09:24:43 -0700

    Update runmany to work in parallel

--- bin/runmany.sh
+++ bin/runmany.sh
@@ -1,17 +1,25 @@
-#!/bin/bash
-# Usage: runmany N <command> <args>...
+#!/bin/bash -x
+# Usage: runmany N P <command> <args>...
+# N - the number of times to run the test
+# P - if 1, run the tests in parallel
 # TODOs:
 # - Convert to Python3.
 # - Handle console output in a sane way.
 # - (Maybe) allow simultaneous execution.
 numtimes=$1
+parallel=$2
 numpass=0
 numfail=0
+shift
 shift
 for i in $(seq 1 1 $numtimes)
 do
   echo "   ============>>>> $i/$numtimes <<<<============"
-  $*
+  if [ $parallel == "1" ]; then
+    $* &
+  else
+    $*
+  fi
   if [ $? != 0 ]; then
     echo "   ============>>>> $i/$numtimes FAILED! <<<<============"
     numfail=$(($numfail + 1))
